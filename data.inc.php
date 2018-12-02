27df545b587591d43815012ba1d5b815043b5e43
triplg3 <triplg3@gmail.com>
2018-12-02 00:49:05 -0500

    Lmao, ned this data file

---
+++ data.inc.php
@@ -0,0 +1,32 @@
+<?php
+
+$Names = array('ASUS GeForce GTX 1060 6GB Dual-Fan OC Edition VR Ready Dual HDMI DP 1.4 Gaming Graphics Card (DUAL-GTX1060-O6G)','EVGA GeForce GTX 1050 Ti Gaming, 4GB GDDR5, DX12 OSD Support (PXOC) Graphics Card 04G-P4-6251-KR','Gigabyte Geforce GTX 1050 2GB GDDR5 128 Bit PCI-E Graphic Card (GV-N1050OC-2GD)','MSI Gaming GeForce GTX 1050 Ti 4GB GDRR5 128-bit HDCP Support DirectX 12 TORX 2.0 Fan Graphics Card (GTX 1050 TI Gaming X 4G)',
+'AMD FD8350FRHKBOX FX-8350 FX-Series 8-Core Black Edition Processor', 'Intel Core i5-8600K Desktop Processor 6 Cores up to 4.3 GHz unlocked LGA 1151 300 Series 95W', 'AMD Ryzen 5 2600 Processor with Wraith Stealth Cooler - YD2600BBAFBOX', 'AMD Ryzen 7 2700X Processor with Wraith Prism LED Cooler - YD270XBGAFBOX',
+'Cooler Master Hyper 212 Evo (RR-212E-20PK-R2) CPU Cooler with PWM Fan, Four Direct Contact Heat Pipes', 'EVGA CLC 280 Liquid/Water CPU Cooler, RGB LED Cooling 400-HY-CL28-V1', 'ORSAIR HYDRO Series H100i PRO RGB AIO Liquid CPU Cooler, 240mm Radiator, Dual 120mm ML Series PWM Fans', 'CORSAIR Hydro Series H60 AIO Liquid CPU Cooler, 120mm Radiator, 120mm SP Series PWM Fan',
+'ASUS ROG STRIX B350-F GAMING AMD Ryzen AM4 DDR4 HDMI DisplayPort M.2 USB 3.1 ATX B350 Motherboard', 'ASUS PRIME Z370-A LGA1151 DDR4 DP HDMI DVI M.2 USB 3.1 Z370 ATX Motherboard with USB 3.1 for 8th Generation Intel Core Processors', 'EVGA 134-KS-E379-KR Classified Intel HDMI Motherboard', 'ASUS ROG Strix Z370-H Gaming LGA1151 (Intel 8th Gen) DDR4 HDMI DVI M.2 Z370 ATX Motherboard with Gigabit LAN and USB 3.1');
+$Types = array('Graphics card','Graphics card','Graphics card','Graphics card','CPU','CPU','CPU','CPU','cooler','cooler','cooler','cooler','motherboard','motherboard','motherboard','motherboard');
+$Prices = array(296,170,130,180,78,225,198,330,27,100,100,60,115,170,159);
+$Descs = array('Powered by NVIDIA Pascal, the ASUS Dual-fan GTX 1060 delivers cool overclocked performance with a 1809MHz Boost Clock and patented wing-blade fans shrouded in white to match a variety of ASUS Signature Series Motherboards.',
+'The EVGA GeForce GTX 1050 Ti hits the perfect spot for that upgrade you know you need, but at the price you want! With the latest NVIDIA Pascal architecture, the 4GB GTX 1050 Ti displays stunning visuals and great performance at 1080p HD+.',
+'This GIGABYTE Geforce GTX 1050 2GB OC graphic card is made for the gaming enthusiasts. Equipped with one of the advanced architecture, get ready to play some of the most graphic intensive games in HD quality.',
+'Pascal is built to meet the demands of next generation displays, including ultra-high-resolution and multiple monitors. It features NVIDIA GameWorks technologies for extremely smooth gameplay and cinematic experiences. Plus, it includes revolutionary new ',
+'AMD FX 8-Core Processor, Unlocked, Black Edition. AMD\'s next-generation architecture takes 8-core processing to a new level. Get up to 24% better frame rates in some of the most demanding games, at stunning resolutions.',
+'Outstanding gaming experiences extend beyond personal gameplay to your entire gaming community. Share those experiences by live-streaming or recording, editing, and posting your epic highlights.',
+'AMD Ryzen 5 2600 Processor with Wraith Stealth Cooler', 'https://www.amazon.com/AMD-Processor-Wraith-Stealth-Cooler/dp/B07B41WS48/ref=sr_1_7?ie=UTF8&qid=1543469450&sr=8-7&keywords=cpu',
+'AMD Ryzen 7 2700X Processor with Wraith Prism LED Cooler. Note that 300-series motherboards may require a BIOS flash to be compatible with Ryzen™ Desktop 2000-series processors',
+'Cooler Master, an industry leading chassis, thermal solution, peripheral, and accessory manufacturer, signals the rebirth of a household name in computing, the Hyper 212 EVO CPU Cooler.',
+'The EVGA CLC 280 LIQUID CPU Cooler has arrived! This new liquid cooler from EVGA gives you incredible performance, low noise, and robust software controls. ',
+'The CORSAIR Hydro Series H100i PRO is an all-in-one RGB liquid CPU cooler with a 240mm radiator built for low-noise cooling performance and bold styling with an RGB LED pump head.',
+'The Hydro Series H60 is an all-in-one liquid CPU cooler with a 120mm radiator built for low-noise liquid CPU cooling and bold styling with a white LED-lit pump head.',
+'Experience next-level performance with the ROG STRIX B350-F GAMING, powered by AMD Ryzen AM4 processors. Unlock your system\'s full potential with 5-Way Optimization featuring SafeSlot and Fan Xpert 4 while personalizing your build with AURA Sync RGB light',
+'Designed for 8th generation Intel Core processors, the Prime Z370-A delivers maximum performance with customizable style. 5-Way Optimization provides intelligent auto-tuning and dynamic fan calibration, AURA Sync RGB with addressable headers and 3D printi',
+'Z370 CLASSIFIED K MB INTEL SOCKET LGA1151 WITH DDR4',
+'Fully command your gaming experience on 8th generation Intel Core processors with the ROG STRIX Z370-H GAMING featuring 5-Way Optimization for dynamic system control and Gamer\'s Guardian for hardware level safeguards like Safe Slot and LANGuard. Dual M.2,'
+);
+$Imgs = array('images/51T1GaSIVLL.jpg', 'images/04G-P4-6251-KR_LG_1.png', 'images/gigabyte1050.jpg', 'images/81VBPk5pnLL._SL1500_.jpg', 
+'images/41OmFJWZ6zL.jpg', 'images/61gj8i-OK0L._SL1200_.jpg', 'images/615WxvyEIBL._SL1182_.jpg', 'images/61IsA3O32GL._SL1178_.jpg', 
+'images/81c-RAt5Y9L._SL1500_.jpg', 'images/819zWLBQfkL._SL1500_.jpg', 'images/71UUKDJ1wsL._SL1500_.jpg', 'images/515bLaqKqzL._SL1000_.jpg',
+'images/81gVeyoJ3QL._SL1365_.jpg', 'images/915nLz6VLVL._SL1500_.jpg', 'images/714bd8HQYOL._SL1200_.jpg', 'images/91JabUCY4aL._SL1500_.jpg'
+);
+
+?>
