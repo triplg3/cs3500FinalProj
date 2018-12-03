56e1f2a9dc8e7f3c0995071fea0cf04c3c44451c
triplg3 <triplg3@gmail.com>
2018-12-02 20:30:20 -0500

    Update Cart.php

--- Cart.php
+++ Cart.php
@@ -76,9 +76,13 @@ $order = <<<ORDER
 					<div class='panel-body'><img src='{$img}' class='img-responsive' style='width:100%' alt='Image'></div>
 					<div class='panel-footer'>
 						<div class='container'>
-						{$name}  
-						<hr>Price:&curren {$price}  <a href='Product.php?varname={$id}'>Visit Store Page</a>    
-						<a href="remove.php?varname={$id}"> <div class="col-sm-2"><input type="submit" value="Remove From Cart" class="btn btn-danger btn-block btn-lg"></div></a>
+							{$name}  
+							<div class='col-sm-2>
+								<hr>Price:&curren {$price}  <a href='Product.php?varname={$id}'>Visit Store Page</a>    
+							</div>
+							<div class='col-sm-4>
+								<a href="remove.php?varname={$id}"> <div class="col-sm-2"><input type="submit" value="Remove From Cart" class="btn btn-danger btn-block btn-lg"></div></a>
+							</div>
 						</div>
 					</div>
 				</div>
